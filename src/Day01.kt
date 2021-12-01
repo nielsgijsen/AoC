41116db76614dfaa2d51f7deee0dde9be7ca36a9
nielsgijsen <nielsgijsen@bol.com>
2021-12-01 12:54:37 +0100

    use zipWithNext instead of for loop

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,25 +1,20 @@
 fun main() {
+
+    fun numberOfLargerNextElements(list: List<Int>): Int {
+        return list.zipWithNext { a, b ->
+            if (a < b) 1 else 0
+        }.sum()
+    }
+
     fun part1(input: List<String>): Int {
-        var count = 0
-        val inputInterger = input.map { it.toInt() }
-        for (i in 1..inputInterger.size-1){
-            if(inputInterger[i] > inputInterger[i-1]){
-                count++
-            }
-        }
-        return count
+        val inputInt = input.map { it.toInt() }
+        return numberOfLargerNextElements(inputInt)
     }
 
     fun part2(input: List<String>): Int {
-        var count = 0
         val inputInt = input.map { it.toInt() }
         val movingWindow = inputInt.windowed(3, 1) { it.sum() }
-        for (i in 0..movingWindow.size-2){
-            if (movingWindow[i] < movingWindow[i+1])
-                count++
-        }
-        println(movingWindow)
-        return count
+        return numberOfLargerNextElements(movingWindow)
     }
 
     // test if implementation meets criteria from the description, like:
